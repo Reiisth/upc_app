a631447a82fb4d757fcbcdb2d97a55c0579fbdc2
Jaspher Andrei A. Malabanan <160213452+Reiisth@users.noreply.github.com>
2026-07-19 23:58:42 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/splash_page.dart';
 import 'theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const UpcConnectApp());
 }
 
